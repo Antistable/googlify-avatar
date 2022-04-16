a35a013c9e3eb153b77e37f28d9e5e9eaa79577e
Antistable <7mint1tea@gmail.com>
2022-04-16 10:53:22 +0800

    complete paint function

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import React, { createRef, TouchEvent, MouseEvent, RefObject } from 'react';
 import './App.css';
 
 enum Stage {
-  Outline = 0,
+  Outline,
   Color,
 }
 
 class App extends React.Component<any, {
   stage: Stage,
-  color: string,
 }> {
 
   canvas: RefObject<HTMLCanvasElement> = createRef();
@@ -17,14 +16,17 @@ class App extends React.Component<any, {
 
   colors = new Map([
     ['r', '#EB4334'],
-    ['g', '#FFCE00'],
-    ['b', '#00A8F0'],
-    ['y', '#F0F000']
+    ['g', '#35AA53'],
+    ['b', '#4286F3'],
+    ['y', '#FAC230']
   ])
+
+  outlinedImageColorArray = document.createElement('canvas').getContext('2d')?.getImageData(0, 0, 1, 1).data;
+
+  color = '#bfbfbf'
 
   state = {
     stage: Stage.Outline,
-    color: '#bfbfbf',
   }
 
   render() {
@@ -32,13 +34,18 @@ class App extends React.Component<any, {
       <>
         {
           this.state.stage === Stage.Outline ? (
-            <img src={require('./img/forward.png')} onClick={() => this.nextStage()} />
+            <img
+              src={require('./img/forward.png')}
+              onClick={() => this.goColor()}
+              className='button'
+            />
           ) : (
             ['r', 'g', 'b', 'y'].map(color =>
               <img
                 key={color}
                 src={require(`./img/${color}.png`)}
                 onClick={() => this.changeColor(color)}
+                className='button'
               />
             )
           )
@@ -62,14 +69,14 @@ class App extends React.Component<any, {
   touch(e: TouchEvent) {
     const x = e.touches[0].clientX;
     const y = e.touches[0].clientY;
-    this.draw(x - 40, y - 40);
+    this.draw(x - (this.canvas.current?.offsetLeft as number), y - (this.canvas.current?.offsetTop as number));
   }
 
   mouse(e: MouseEvent) {
     if (e.buttons === 1) {
       const x = e.clientX;
       const y = e.clientY;
-      this.draw(x - 40, y - 40);
+      this.draw(x - (this.canvas.current?.offsetLeft as number), y - (this.canvas.current?.offsetTop as number));
     }
   }
 
@@ -77,17 +84,28 @@ class App extends React.Component<any, {
     const context = this.canvas.current?.getContext('2d') as CanvasRenderingContext2D;;
     context.beginPath();
     context.arc(x, y, 20, 0, 2 * Math.PI);
-    context.fillStyle = this.state.color;
+    context.fillStyle = this.color;
     context.fill();
     context.closePath();
+    if (this.state.stage === Stage.Color) {
+      let coloredImageData = this.canvas.current?.getContext('2d')?.getImageData(0, 0, this.canvas.current.width, this.canvas.current.height) as ImageData;
+      let colorsArray = coloredImageData.data;
+      for (let index = 0; index < colorsArray.length; index++) {
+        if ((this.outlinedImageColorArray as Uint8ClampedArray)[index] === 0) {
+          colorsArray[index] = 0;
+        }
+      }
+      this.canvas.current?.getContext('2d')?.putImageData(coloredImageData, 0, 0);
+    }
   }
 
   changeColor(color: string) {
-    this.setState({ color: this.colors.get(color) as string });
+    this.color = this.colors.get(color) as string;
   }
 
-  nextStage() {
-    this.setState(state => { return { stage: state.stage + 1 } });
+  goColor() {
+    this.setState({ stage: Stage.Color });
+    this.outlinedImageColorArray = this.canvas.current?.getContext('2d')?.getImageData(0, 0, this.canvas.current.width, this.canvas.current.height).data;
   }
 
 }
